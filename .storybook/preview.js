a8f20bf01467d6ad700d64cf0be024d66ced99b7
tory <tory1327643851@gmail.com>
2020-08-31 17:02:10 +0800

    Avatar组件开发完成，配置路径、增加插件：storybook、hygen，初始化Icon组件

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,6 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
 
-addDecorator((storyFn) => {
-  <ThemeProvider theme={theme}>{storyFn}</ThemeProvider>;
-});
+addDecorator((storyFn) => (
+  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+));
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import theme from './theme';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
   <React.StrictMode>
