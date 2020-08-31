ab1f67380bb5928360535a676c4c2df113c5f8d1
tory <tory1327643851@gmail.com>
2020-08-31 19:28:15 +0800

    完善Icon组件，引用font awesome图标库

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme';
 
+// 增加对styled-component的支持
 addDecorator((storyFn) => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
 ));
+
+// 显示分区
+addParameters({
+  options: {
+    showRoots: true,
+  },
+});
--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,9 +1,41 @@
-import React from "react";
-import Icon from ".";
+import React from 'react';
+import Icon from '.';
+import { ReactComponent as SmileIcon } from 'assets/icon/smile.svg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCommentDots,
+  faFolder,
+  faStickyNote,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default {
-  title: "Icon",
-  component: Icon
+  title: 'UI 组件/Icon',
+  component: Icon,
 };
 
-export const Default = () => <Icon>默认</Icon>;
+export const Default = () => <Icon icon={SmileIcon} />;
+
+export const CustomColor = () => <Icon icon={SmileIcon} color="#cccccc" />;
+
+export const CustomSize = () => (
+  <Icon icon={SmileIcon} width={48} height={48} color="#cccccc" />
+);
+
+export const FontAwesome = () => {
+  return <FontAwesomeIcon icon={faCommentDots} />;
+};
+
+export const FontAwesomeColor = () => {
+  return <FontAwesomeIcon icon={faCommentDots} style={{ color: '#cccccc' }} />;
+};
+
+export const FontAwesomeSize = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faFolder} style={{ fontSize: '24px' }} />
+      <FontAwesomeIcon icon={faStickyNote} style={{ fontSize: '36px' }} />
+      <FontAwesomeIcon icon={faCommentDots} style={{ fontSize: '48px' }} />
+    </div>
+  );
+};
