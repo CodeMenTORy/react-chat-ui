62577a2e7906df4680ab5c04c7442f757de27884
tory <tory1327643851@gmail.com>
2020-09-03 11:10:47 +0800

    Popover组件开发完成

---
+++ src/components/Popover/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import StyledPopover, { Content, Triangle, Target } from './style';
+
+function Popover({
+  children,
+  content,
+  offset = {},
+  onVisible, // 回调
+  onHide, // 回调
+  ...rest
+}) {
+  const [visible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    if (visible) {
+      setVisible(false);
+      onHide && onHide();
+    } else {
+      setVisible(true);
+      onVisible && onVisible();
+    }
+  };
+
+  return (
+    <StyledPopover {...rest} onClick={handleClick}>
+      <Content visible={visible} offset={offset}>
+        {content}
+      </Content>
+      <Triangle visible={visible} offset={offset} />
+      <Target>{children}</Target>
+    </StyledPopover>
+  );
+}
+
+Popover.propTypes = {
+  children: PropTypes.any,
+  content: PropTypes.any,
+  offset: PropTypes.shape({ x: PopTypes.string, y: PropTypes.string }),
+  onVisible: PropTypes.func,
+  onHide: PropTypes.func,
+};
+
+export default Popover;
