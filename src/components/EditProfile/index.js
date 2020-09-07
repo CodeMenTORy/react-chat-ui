6bae45c4cb30bb52c5ad353baecfd10bf5b4e7aa
tory <tory1327643851@gmail.com>
2020-09-07 15:02:14 +0800

    编辑个人资料组件（EditProfile）开发完成

--- src/components/EditProfile/index.js
+++ src/components/EditProfile/index.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import StyledEditProfile from './style';
+import StyledEditProfile, {
+  GroupTitle,
+  GenderAndRegion,
+  SelcetGroup,
+  StyledIconInput,
+} from './style';
 import Profile from 'components/Profile';
+import face from 'assets/images/face-male-1.jpg';
+import Avatar from 'components/Avatar';
+import Button from 'components/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import 'styled-components/macro';
+import InputText from 'components/Input/InputText';
+import Radio from 'components/Radio';
+import LabelContainer from 'components/LabelContainer';
+import Select from 'components/Select';
+import Option from 'components/Option';
+import Icon from 'components/Icon';
+import {
+  faWeibo,
+  faGithub,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
 
 function EditProfile({ children, ...rest }) {
   const [showEdit, setShowEdit] = useState(false);
@@ -14,7 +36,70 @@ function EditProfile({ children, ...rest }) {
       />
     );
   }
-  return <StyledEditProfile {...rest}>{children}</StyledEditProfile>;
+  return (
+    <StyledEditProfile {...rest}>
+      <Avatar
+        src={face}
+        size="160px"
+        css={`
+          grid-area: 1 / 1 / 2 / 2;
+          justify-self: center;
+          margin-bottom: 12px;
+        `}
+      />
+      <Button
+        size="52px"
+        css={`
+          grid-area: 1 / 1 / 3 / 2;
+          align-self: end;
+          justify-self: end;
+          z-index: 10;
+        `}
+      >
+        <FontAwesomeIcon icon={faCheck} onClick={() => setShowEdit(false)} />
+      </Button>
+      <GroupTitle>基本信息</GroupTitle>
+      <InputText lable="昵称" />
+      <GenderAndRegion>
+        <Radio.Group label="性别">
+          <Radio name="gender">男</Radio>
+          <Radio name="gender">女</Radio>
+        </Radio.Group>
+        <LabelContainer label="地区">
+          <SelcetGroup>
+            <Select type="form">
+              <Option>省份</Option>
+            </Select>
+            <Select type="form">
+              <Option>城市</Option>
+            </Select>
+            <Select type="form">
+              <Option>区县</Option>
+            </Select>
+          </SelcetGroup>
+        </LabelContainer>
+      </GenderAndRegion>
+      <InputText label="个性签名" />
+      <GroupTitle>联系信息</GroupTitle>
+      <InputText label="联系电话" />
+      <InputText label="电子邮箱" />
+      <InputText label="个人网站" />
+
+      <GroupTitle>社交信息</GroupTitle>
+      <IconInput icon={faWeibo} bgColor="#F06767" />
+      <IconInput icon={faGithub} bgColor="black" />
+      <IconInput icon={faLinkedinIn} bgColor="#2483C0" />
+    </StyledEditProfile>
+  );
+}
+
+function IconInput({ icon, bgColor, ...rest }) {
+  return (
+    <StyledIconInput>
+      <Icon.Social icon={icon} bgColor={bgColor} />
+      <InputText {...rest} />
+    </StyledIconInput>
+  );
 }
 
 EditProfile.propTypes = {
