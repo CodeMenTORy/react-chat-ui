9a310ffe3231ea379d3032d7042e1810c1db5168
tory <tory1327643851@gmail.com>
2020-09-17 14:43:50 +0800

    react-router 的基本配置以及导航栏路由的实现

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -14,18 +14,20 @@ import {
   faEllipsisH,
   faCog,
 } from '@fortawesome/free-solid-svg-icons';
+import { Link, matchPath, useLocation } from 'react-router-dom';
 
 function NavBar({ ...rest }) {
   return (
     <StyledNavBar {...rest}>
       <Avatar src={profileImage} status="online" />
       <MenuItems>
-        <MenuItem showBadge active icon={faCommentDots} />
-        <MenuItem icon={faUsers} />
-        <MenuItem icon={faFolder} />
-        <MenuItem icon={faStickyNote} />
+        <MenuItem to="/" showBadge icon={faCommentDots} />
+        <MenuItem to="/contacts" icon={faUsers} />
+        <MenuItem to="/files" icon={faFolder} />
+        <MenuItem to="/notes" icon={faStickyNote} />
         <MenuItem icon={faEllipsisH} />
         <MenuItem
+          to="/settings"
           icon={faCog}
           css={`
             align-self: end;
@@ -36,14 +38,20 @@ function NavBar({ ...rest }) {
   );
 }
 
-function MenuItem({ icon, active, showBadge, ...rest }) {
+function MenuItem({ to, icon, showBadge, ...rest }) {
+  // 获取当前路由的location
+  const loc = useLocation();
+  const active = !!matchPath(loc.pathname, {
+    path: to,
+    exact: to === '/',
+  });
   return (
     <StyledMenuItem active={active} {...rest}>
-      <a href="https://www.google.com">
+      <Link to={to}>
         <Badge show={showBadge}>
           <MenuIcon active={active} icon={icon} />
         </Badge>
-      </a>
+      </Link>
     </StyledMenuItem>
   );
 }
